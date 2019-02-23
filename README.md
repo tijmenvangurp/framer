cdbc0c421f907f685b422484acf2b12ebd3f1993
Gurp, Tijmen van <tijmen.van.gurp@philips.com>
2019-02-23 13:40:51 +0100

    First API state change working

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# install
+# Before you begin
+
+ clone this repository, and add the file extension .framerx to the cloned repository folder. This will enable you to open the project as framerX file.
+
+## install
 
 npm install
 
--- code/api_connection.tsx
+++ code/api_connection.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { PropertyControls, ControlType } from "framer";
+import axios from "axios";
 
 // For the best editing experience in VSCode, install Prettier
 // https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
@@ -16,7 +17,7 @@ export class api_connection extends React.Component<Props> {
   // Return the component contents in JSX
   // https://reactjs.org/docs/introducing-jsx.html
   render() {
-    return <div style={style}>{this.props.text}</div>;
+    return <div style={{}}>{this.state.name}</div>;
   }
 
   // Set default values for props if there are none
@@ -30,16 +31,36 @@ export class api_connection extends React.Component<Props> {
   static propertyControls: PropertyControls<Props> = {
     text: { type: ControlType.String, title: "Text" }
   };
+
+  state = {
+    name: ""
+  };
+
+  componentDidMount() {
+    let namesUrl = "https://listsdesign.herokuapp.com/lists/names-nl_NL.json";
+    axios
+      .get(`https://cors-anywhere.herokuapp.com/${namesUrl}`)
+      .then(response => {
+        const randomNumber = getRandomInt(100);
+        const randomName =
+          response.data["Names (Netherlands)"][randomNumber].data;
+        this.setState({ name: randomName });
+      });
+  }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 // Define some standard CSS for your component
-const style: React.CSSProperties = {
-  height: "100%",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  textAlign: "center",
-  color: "#8855FF",
-  background: "rgba(136, 85, 255, 0.1)",
-  overflow: "hidden"
-};
+// const style: React.CSSProperties = {
+//   height: "100%",
+//   display: "flex",
+//   alignItems: "center",
+//   justifyContent: "center",
+//   textAlign: "center",
+//   color: "#8855FF",
+//   background: "rgba(136, 85, 255, 0.1)",
+//   overflow: "hidden"
+// };
